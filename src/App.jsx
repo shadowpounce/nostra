613aa945fd0549b5a105f2bc21532d9e4aa59aa2
Ilya Minkin <shadowpounce@icloud.com>
2023-01-19 20:13:30 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  )
-}
+const App = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: 'about',
+    element: <About />,
+  },
+])
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './assets/styles/index.scss'
 
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={App} />
   </React.StrictMode>
 )
